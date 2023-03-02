c2421d96701e4186334b5b21ce0c447d297b4e3e
Samee Vohra <samee.vohra@gmail.com>
2023-03-02 15:42:04 -0800

    added unstyled Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <div>
+      <h2>Any Questions? Contact us.</h2>
+      <p>I look forward to hearing from you regarding any questions or feedback.</p>
+      <a href="https://www.linkedin.com/in/sameev/">LinkedIn</a>
+      <a href="https://github.com/sameev">GitHub</a>
+    </div>
+  )
+}
