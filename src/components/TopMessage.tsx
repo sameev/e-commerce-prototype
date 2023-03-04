22cafff6fc1dc792eca9705675f7a376e267384e
Samee Vohra <samee.vohra@gmail.com>
2023-03-03 22:39:11 -0800

    added topmessage and updated styles

---
+++ src/components/TopMessage.tsx
@@ -0,0 +1,9 @@
+export default function TopMessage() {
+  return (
+    <div className="promotional-message">
+      <h3>DISCOVER</h3>
+      <h2>Coffee</h2>
+      <p>Seeking <strong>quality, truth and accountability</strong> in coffee. We journey to find the finest and most unique coffees in the world.</p>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import ProductList from '@components/components/ProductList';
 import Head from 'next/head';
 import { IProduct } from '../components/Product';
-import Contact from '../components/Contact.jsx';
+import Contact from '../components/Contact';
 import { products } from '../lib/products';
 import { GetStaticProps } from 'next';
 import Script from 'next/script';
+import TopMessage from '@components/components/TopMessage';
 
 interface IProductListProps {
   products: IProduct[];
@@ -26,6 +27,7 @@ export default function Home({ products }: IProductListProps) {
         <link rel='preconnect' href='<https://cdn.snipcart.com>' />
       </Head>
       <main className='main'>
+        <TopMessage />
         <ProductList products={products} />
         <Contact />
       </main>
